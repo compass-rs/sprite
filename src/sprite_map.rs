1e040008c0fb61f9b8a523d960784338780d0359
Marius Seritan <github@seritan.net>
2015-04-06 21:26:55 -0700

    Compute dimensions of the sprite map

--- src/sprite_map.rs
+++ src/sprite_map.rs
@@ -2,12 +2,16 @@
 
 use std::fs;
 use std::path::Path;
+use image_lib;
+use std::cmp;
+use image_lib::GenericImage;
 
+// Hold information about a built sprite map.
 pub struct SpriteMap {
-    width: u32,
-    height: u32,
-    names: Vec<String>,
-    url: String
+    pub width: u32,
+    pub height: u32,
+    pub names: Vec<String>,
+    pub url: String
 }
 
 impl SpriteMap {
@@ -17,15 +21,27 @@ impl SpriteMap {
         let paths = fs::read_dir(&Path::new(glob)).unwrap();
 
         let mut names:Vec<String> = vec!();
+        let mut width = 0;
+        let mut height = 0;
         for entry in paths {
             let path = entry.unwrap().path();
             if path.extension().unwrap() == "png" {
-                names.push(path.file_stem().unwrap().to_str().unwrap().to_string())
+                match image_lib::open(&path) {
+                    Ok(img) => {
+                        let dimensions = img.dimensions();
+                        width = width + dimensions.0;
+                        height = cmp::max(height, dimensions.1);
+                        names.push(path.file_stem().unwrap().to_str().unwrap().to_string())
+                    },
+                    Err(_) => {
+
+                    }
+                }
             }
         }
         SpriteMap {
-            width: 0,
-            height: 0,
+            width: width,
+            height: height,
             names: names,
             url: glob.to_string()
         }
@@ -43,5 +59,7 @@ fn builds() {
     let sprite_map = SpriteMap::build("data/my-icons");
     let mut names = sprite_map.names;
     names.sort();
-    assert_eq!(names,vec!("edit1","edit2","edit3"))
+    assert_eq!(names,vec!("edit1","edit2","edit3"));
+    assert_eq!(sprite_map.height, 32 as u32);
+    assert_eq!(sprite_map.width, (names.len()*32) as u32);
 }
